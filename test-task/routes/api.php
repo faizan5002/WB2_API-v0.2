09305596351031e4c6a365694689c2b070e39c94
faizan5002 <92327905+faizan5002@users.noreply.github.com>
2024-09-20 16:52:31 +0500

    Update api.php

--- test-task/routes/api.php
+++ test-task/routes/api.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\AgentController;
 Route::prefix('v1')->group(function(){
     Route::post('agent/create', [AgentController::class, 'createAgent']);
     Route::post('agent/password', [AgentController::class, 'modifyPassword']);
-    
+    Route::post('agent/update', [AgentController::class, 'updateAgentAccount']);
+    Route::get('agent/list',[AgentController::class,'getAgentList']);
 });
 
